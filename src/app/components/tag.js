3d977cb90db521762021ad98cdc5b817d75346e7
Andre <andre.sousa@am4.com.br>
2019-04-13 11:50:10 -0300

    some changes and active check

--- src/app/components/tag.js
+++ src/app/components/tag.js
@@ -7,17 +7,20 @@ export default class Tag extends React.Component {
     constructor() {
         super();
         this.state = {
-            data: ''
+            data: '',
+            active: false
         };
         this.getData = this.getData.bind(this)
+        this.shouldRender = this.shouldRender.bind(this)
     }
 
     async getData() {
-
         let getProduct = document.getElementById("listagemProdutos")
         let productUl = getProduct.getElementsByTagName("ul")
         let testes = [...productUl].filter(q => q.dataset.produtosLinha !== undefined)
         let newImage = new Image(100, 100)
+
+        // mudar a origem da imagem para a hospedagem da API
         newImage.src = "https://www.hollidaystore.com.br/hollidaystore/painel/painel/public/images/tag-lancamento-PNG-150x199px-1.png"
 
         let test_url = window.location.href
@@ -56,7 +59,7 @@ export default class Tag extends React.Component {
 
                                 })
                             })
-                        }, 200)
+                        }, 10)
                     })
                 }
 
@@ -64,12 +67,39 @@ export default class Tag extends React.Component {
             .catch(err => console.log("erro", err))
     }
 
+    shouldRender() {
+        let test_url = window.location.href
+        let url = (api.LOCATION_URL == test_url ? api.CHECK_ACTIVE : dev_api.DEV_CHECK_ACTIVE);
+        var active = false
+        return fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                let active = result[0]
+                if (this._isMounted) {
+                    if (active.active === "1") {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            })
+            .catch(err => console.log("error", err))
+    }
+
     componentWillUnmount() {
         this._isMounted = false
     }
     componentDidMount() {
         this._isMounted = true
-        this.getData()
+        this.shouldRender()
+            .then(r => {
+                if (r)
+                    this.getData()
+            })
+
+
+
+
 
     }
 
--- src/app/config/config.js
+++ src/app/config/config.js
@@ -9,15 +9,17 @@ export const api = {
     AVAL_READ: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/avaliacoes/read.php',
     AVAL_CREATE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/avaliacoes/create.php',
     TRINTA_READ: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/read_api_holliday.php',
+    CHECK_ACTIVE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/check_active.php'
 
 }
 
 export const dev_api = {
     LOCATION_URL: "http://localhost:8080/",
-    DEV_API_CORE: "http://localhost/holliday-painel-api/api/",
-    DEV_QTD_READ: 'http://localhost/holliday-painel-api/api/quantidade/read.php',
-    DEV_QTD_UPDATE: 'http://localhost/holliday-painel-api/api/quantidade/update.php',
-    DEV_AVAL_READ: 'http://localhost/holliday-painel-api/api/avaliacoes/read.php',
-    DEV_AVAL_CREATE: 'http://localhost/holliday-painel-api/api/avaliacoes/create.php',
-    DEV_TRINTA_READ: 'http://localhost/holliday-painel-api/api/tag/read_api_holliday.php',
+    DEV_API_CORE: "http://localhost/holliday-painel/api/",
+    DEV_QTD_READ: 'http://localhost/holliday-painel/api/quantidade/read.php',
+    DEV_QTD_UPDATE: 'http://localhost/holliday-painel/api/quantidade/update.php',
+    DEV_AVAL_READ: 'http://localhost/holliday-painel/api/avaliacoes/read.php',
+    DEV_AVAL_CREATE: 'http://localhost/holliday-painel/api/avaliacoes/create.php',
+    DEV_TRINTA_READ: 'http://localhost/holliday-painel/api/tag/read_api_holliday.php',
+    DEV_CHECK_ACTIVE: 'http://localhost/holliday-painel/api/tag/check_active.php'
 }
