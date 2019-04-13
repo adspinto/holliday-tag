1e06f350a4c158ff6a726716dc1e0d2f375594b6
Andre <andre.sousa@am4.com.br>
2019-04-13 20:11:40 -0300

    get product id from page

--- src/app/components/tag.js
+++ src/app/components/tag.js
@@ -14,7 +14,7 @@ export default class Tag extends React.Component {
         this.shouldRender = this.shouldRender.bind(this)
     }
 
-    async getData() {
+    getData() {
         let getProduct = document.getElementById("listagemProdutos")
         let productUl = getProduct.getElementsByTagName("ul")
         let testes = [...productUl].filter(q => q.dataset.produtosLinha !== undefined)
@@ -25,9 +25,9 @@ export default class Tag extends React.Component {
 
         let test_url = window.location.href
         let url = (api.LOCATION_URL == test_url ? api.TRINTA_READ : dev_api.DEV_TRINTA_READ);
+        let arr_data = []
 
-
-        await fetch(url)
+        fetch(url)
             .then(response => {
                 return response.json()
             })
@@ -45,13 +45,13 @@ export default class Tag extends React.Component {
 
                                 arr.map((ar) => {
                                     let idContainer = ar.querySelector(".listagem-item").classList[1].slice(8)
+                                    arr_data.push(idContainer)
                                     let testContainer = ar.querySelector(".imagem-produto")
                                     let testeDivCreation = document.createElement("img");
                                     testeDivCreation.src = newImage.src
                                     testeDivCreation.classList.add("tag-image")
                                     for (let i = 0; i < data.length; i++) {
                                         if (data[i].prod_trinta_dias === idContainer) {
-
                                             testContainer.appendChild(testeDivCreation)
                                         }
 
@@ -64,7 +64,44 @@ export default class Tag extends React.Component {
                 }
 
             })
+            .then((response) => {
+                let get_url = (api.LOCATION_URL == test_url ? api.GET_PROMO : dev_api.DEV_GET_PROMO);
+                fetch(get_url)
+                    .then(response => response.json())
+                    .then(result => {
+                        let day = 24 * 60 * 60
+                        let date = (new Date().getTime() / 1000) * 10
+                        if (result.length > 0)
+                            date = new Date(result[0].created).getTime() / 1000
+                        let today = new Date().getTime() / 1000
+                        let diferenca = Math.abs(Math.round(date - today))
+                        if (diferenca >= day) {
+                            setTimeout(() => {
+                                console.log(arr_data)
+                                let post_url = (api.LOCATION_URL == test_url ? api.UPDATE_PROMO : dev_api.DEV_UPDATE_PROMO);
+                                let tr_url = (api.LOCATION_URL == test_url ? api.TR_PROMO : dev_api.DEV_TR_PROMO);
+                                fetch(tr_url)
+                                    .then(response => {
+                                        arr_data.map(ar => {
+                                            let objTest = { prod_promo: ar }
+                                            fetch(post_url, {
+                                                method: 'POST',
+                                                body: JSON.stringify(objTest)
+                                            })
+                                                .catch(err => console.log("erro", err))
+
+                                        })
+                                    })
+                                    .catch(err => console.log(err, "erro"))
+
+                            }, 100)
+
+                        }
+                    })
+                    .catch(err => console.log("erro", err))
+            })
             .catch(err => console.log("erro", err))
+
     }
 
     shouldRender() {
--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,7 +1,7 @@
 // global variables
 
 export const api = {
-    LOCATION_URL: "https://atacado.hollidaystore.com.br/pagina/pesquisa-de-satisfacao.html",
+    LOCATION_URL: "https://atacado.hollidaystore.com.br/",
     TAG_LOCATION_URL: "https://atacado.hollidaystore.com.br/pagina/pesquisa-de-satisfacao.html",
     API_CORE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/',
     QTD_READ: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/quantidade/read.php',
@@ -9,7 +9,10 @@ export const api = {
     AVAL_READ: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/avaliacoes/read.php',
     AVAL_CREATE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/avaliacoes/create.php',
     TRINTA_READ: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/read_api_holliday.php',
-    CHECK_ACTIVE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/check_active.php'
+    CHECK_ACTIVE: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/check_active.php',
+    GET_PROMO: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/read_promo.php',
+    UPDATE_PROMO: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/update_promo.php',
+    TR_PROMO: 'https://www.hollidaystore.com.br/hollidaystore/painel/api/tag/truncate_promo.php'
 
 }
 
@@ -21,5 +24,8 @@ export const dev_api = {
     DEV_AVAL_READ: 'http://localhost/holliday-painel/api/avaliacoes/read.php',
     DEV_AVAL_CREATE: 'http://localhost/holliday-painel/api/avaliacoes/create.php',
     DEV_TRINTA_READ: 'http://localhost/holliday-painel/api/tag/read_api_holliday.php',
-    DEV_CHECK_ACTIVE: 'http://localhost/holliday-painel/api/tag/check_active.php'
+    DEV_CHECK_ACTIVE: 'http://localhost/holliday-painel/api/tag/check_active.php',
+    DEV_GET_PROMO: 'http://localhost/holliday-painel/api/tag/read_promo.php',
+    DEV_UPDATE_PROMO: 'http://localhost/holliday-painel/api/tag/update_promo.php',
+    DEV_TR_PROMO: 'http://localhost/holliday-painel/api/tag/truncate_promo.php'
 }
